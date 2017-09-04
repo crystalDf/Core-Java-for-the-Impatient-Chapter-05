6e678bd342c9f9e949828b9ec44d58241f1c2ead
crystalDf <chendong333@gmail.com>
2017-09-04 23:25:54 +0800

    Chapter 05

--- src/ch05/sec01/RethrowingAndChainingExceptions.java
+++ src/ch05/sec01/RethrowingAndChainingExceptions.java
@@ -4,13 +4,20 @@ import com.oracle.tools.packager.Log;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class RethrowingAndChainingExceptions {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
         read("words");
+
+        try {
+            throw new SQLException();
+        } catch (SQLException e) {
+            throw new ClassNotFoundException();
+        }
     }
 
     public static void read(String filename) throws IOException {
