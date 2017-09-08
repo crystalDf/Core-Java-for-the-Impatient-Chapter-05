6f435ddc3298ce8c886b0eccb08a1f31eb58e03a
crystalDf <chendong333@gmail.com>
2017-09-08 22:54:16 +0800

    Chapter 05

--- src/ch05/sec03/LoggerTest.java
+++ src/ch05/sec03/LoggerTest.java
@@ -1,5 +1,6 @@
 package ch05.sec03;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class LoggerTest {
@@ -9,5 +10,14 @@ public class LoggerTest {
         Logger.getGlobal().info("Hello World.");
 
         Logger logger = Logger.getLogger("com.star.myapp");
+
+        logger.setLevel(Level.FINE);
+
+        logger.warning("Hello");
+        logger.fine("World");
+
+        Level level = Level.SEVERE;
+
+        logger.log(level, "Hello World.");
     }
 }
